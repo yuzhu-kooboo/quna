4df7f73dc88767dce8a5095a06c4f015100e0c36
zhuyuzhu <struggle_zhu@163.com>
2018-11-24 16:49:48 +0800

    finish swiper component

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,19 @@
 <template>
 <div>
   <home-header></home-header>
+  <home-swiper></home-swiper>
 </div>
 
 </template>
 <script>
 import HomeHeader from './components/HomeHeader'
+import HomeSwiper from './components/HomeSwiper'
 
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
---
+++ src/pages/home/components/HomeSwiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home-header-swiper">
+    <swiper :options="swiperOption">
+    <swiper-slide v-for="list of swiperList" :key="list.id"><img class="swiper-img" :src="list.imgUrl" alt=""></swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        autoplay: 1500,
+        disableOnInteraction: false
+      },
+      swiperList: [
+        {
+          id: '001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/12/856f100069809e02.jpg_750x200_e3485a2b.jpg'
+        }, {
+          id: '002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/ec/f6967f24afeef202.jpg_750x200_9415719f.jpg'
+        }, {
+          id: '003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/7c/8e5c4ab8ee8b7402.jpg_750x200_dd7a38dd.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .home-header-swiper >>> .swiper-pagination-bullet-active
+    background #fff!important
+  .home-header-swiper
+    width 100%
+    height 0
+    padding-bottom 26.7%
+    overflow hidden
+    .swiper-img
+      width: 100%
+</style>
