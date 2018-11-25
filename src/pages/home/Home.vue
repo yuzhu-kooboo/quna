2afddff68ec748b7fc056d5a6da1e2dc28fab197
zhuyuzhu <struggle_zhu@163.com>
2018-11-25 17:29:29 +0800

    category

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,18 +2,21 @@
 <div>
   <home-header></home-header>
   <home-swiper></home-swiper>
+  <home-recommend></home-recommend>
 </div>
 
 </template>
 <script>
 import HomeHeader from './components/HomeHeader'
 import HomeSwiper from './components/HomeSwiper'
+import HomeRecommend from './components/HomeRecommend'
 
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeRecommend
   }
 }
 </script>
