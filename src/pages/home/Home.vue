2ba697a7c059d337f3685c0300cc38b57c004d20
zhuyuzhu <struggle_zhu@163.com>
2018-11-30 11:04:29 +0800

    fix: finished today recomment and create Tag and ScenicSpot components

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,6 +3,7 @@
   <home-header></home-header>
   <home-swiper></home-swiper>
   <home-recommend></home-recommend>
+  <home-guess></home-guess>
 </div>
 
 </template>
@@ -10,13 +11,15 @@
 import HomeHeader from './components/HomeHeader'
 import HomeSwiper from './components/HomeSwiper'
 import HomeRecommend from './components/HomeRecommend'
+import HomeGuess from './components/HomeGuess'
 
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeRecommend
+    HomeRecommend,
+    HomeGuess
   }
 }
 </script>
---
+++ src/pages/home/components/Tag.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="taglogo">
+    <slot name="today"></slot>
+    <slot name="anytime"></slot>
+    <slot name="tomorrow"></slot>
+    <div v-if="name=='today'" class="tag today">今日预定</div>
+    <div v-else-if="name=='anytime'" class="tag anytime">随买随用</div>
+    <div v-else-if="name=='tomorrow'" class="tag tomorrow">可定明日</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tag',
+  props: ['name']
+}
+</script>
+
+<style lang="stylus" scoped>
+  .taglogo{
+    position: relative;
+    .tag{
+      position: absolute;
+      top: 0px;
+      left: 0px;
+      width: 1.02rem;
+      height: 0.38rem;
+      border-bottom-right-radius: 0.24rem
+      font-size: 0.24rem
+      color: #fff;
+      line-height: 0.4rem
+    }
+    .today{
+      background: green;
+    }
+    .anytime{
+      background: #FF9C13;
+    }
+    .tomorrow{
+      background: #07CEE7;
+    }
+  }
+
+</style>
