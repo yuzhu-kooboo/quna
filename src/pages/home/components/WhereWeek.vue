81bc0e962fb337f76e0f0729115d4c0e3d08ecf8
zhuyuzhu <struggle_zhu@163.com>
2018-11-30 17:02:31 +0800

    fix: Where are we goging on weekend? component: WeekSpot and WhereWeek

---
+++ src/pages/home/components/WhereWeek.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="where">
+    <div class="where-week">周末去哪儿</div>
+    <week-spot v-for="list of whereWeek" :key="list.id" :list="list"></week-spot>
+  </div>
+</template>
+
+<script>
+import WeekSpot from './WeekSpot'
+export default {
+  name: 'WhereWeek',
+  props: {
+    whereWeek: Array,
+    required: true
+  },
+  components: {
+    WeekSpot
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .where {
+    background: #f5f5f5;
+    width: 100%
+    .where-week{
+      height: 0.7rem
+      line-height 0.7rem
+      margin-left 0.2rem
+      color #212121
+    }
+  }
+
+</style>
